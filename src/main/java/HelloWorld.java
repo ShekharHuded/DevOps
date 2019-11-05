27a1b446446d991cb4d418542636d42dbc56894d
ShekharHuded <49373517+ShekharHuded@users.noreply.github.com>
2019-11-04 22:38:35 -0600

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -15,9 +15,13 @@ public class HelloWorld {
 	}
 	
 	public static void main (String[] args) {
+		final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+                final LocalDateTime now = LocalDateTime.now();
+		
 		HelloWorld hello = new HelloWorld() ;
 		hello.getHelloWorld ();
 		hello.getdevproj () ;
+		
 		System.out.println ( "The Current Time is " + dtf.format(now) );
 	}
 }
