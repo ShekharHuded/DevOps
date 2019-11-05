1f45098ba5aed07ce50267e1e30dd4d536aed2b7
ShekharHuded <49373517+ShekharHuded@users.noreply.github.com>
2019-11-04 22:35:19 -0600

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,17 +6,18 @@ import java.time.format.DateTimeFormatter;
 
 public class HelloWorld {
 
-	public static void getHelloWorld (){
+	public static void getHelloWorld () {
 		System.out.println ( "Hello, world :)" ); 
 	}
 
-	public static void getdevproj (){
-		System.out.println ( "The Current Time is " + dtf.format(now) );
+	public static void getdevproj () {
+		System.out.println ( "Completed DevOps Training @ Edureka" ) ;
 	}
 	
 	public static void main (String[] args) {
 		HelloWorld hello = new HelloWorld() ;
 		hello.getHelloWorld ();
 		hello.getdevproj () ;
+		System.out.println ( "The Current Time is " + dtf.format(now) );
 	}
 }
