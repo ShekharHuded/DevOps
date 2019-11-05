5bba84e6cfdc8860fea8be12f8dd60d538d3151a
ShekharHuded <49373517+ShekharHuded@users.noreply.github.com>
2019-11-04 18:49:51 -0600

    Update and Rename the file MessageBuilder.java

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.edureka.project;
+
+public class HelloWorld {
+
+	public static void getHelloWorld (){
+		System.out.println ( "Hello, world :)" ); 
+	}
+
+	public static void devproj (){
+		System.out.println ( "Completed DevOPS Project" );
+	}
+	
+	public static void main (String[] args) {
+		System.out.println("Wel come!! :) ");
+		HelloWorld hello = new HelloWorld() ;
+		hello.getHelloWorld ();
+		hello.devproj () ;
+	}
+}
